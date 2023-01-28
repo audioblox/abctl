c08ce37bbcfce3d19f8e5c285187adb35378fd11
Bas Donker <bas.donker@forma.ai>
2023-01-28 11:36:48 -0500

    Configurable url

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 import os
 
-BASE_URL = "https://api.soundingots.com"
+BASE_URL = os.getenv("ABCTL_BASE_URL") or "https://api.soundingots.com"
 ROOT_DIR = os.getenv("ABCTL_ROOT_DIR") or "~/.abctl"
 USER_FILE = os.path.join(ROOT_DIR, "user")
 WORKDIR_FILE = os.path.join(ROOT_DIR, "workdir")
